f89232e86a0c435b20fdeaa24f3289a7179e5fbe
Rafael Anzini Ximenes <anzinirafael50@gmail.com>
2022-04-28 13:56:23 -0300

    update fauna db

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,42 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { fauna } from "../../../src/Components/Services/fauna";
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
   ],
-})
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        const { email } = user;
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(q.Collection("users"), { data: { email } }), q.Get(
+            q.Match(
+              q.Index("user_by_email"),
+              q.Casefold(user.email)
+           )
+          )
+         )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
